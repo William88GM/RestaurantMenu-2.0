7786c94594eceb746410a049d4fa770147b9b947
Guillermo Martínez <guillermogabrielmartinezbarros@gmail.com>
2024-10-14 15:48:26 -0300

    Update page.jsx

--- restaurant-menu/src/app/login/page.jsx
+++ restaurant-menu/src/app/login/page.jsx
@@ -3,8 +3,12 @@ import { useContext, useEffect } from "react";
 import DynamicHome from "../DynamicHome";
 import { useRouter } from "next/navigation";
 import { SessionContext } from "@/Context/SessionContext";
+export const runtime = 'edge';
 
-export default function page({ }) {
+//force dynamic
+// export const dynamic = "force-dynamic";
+
+export default function Page() {
     const router = useRouter()
     const { logged, setLogged } = useContext(SessionContext)
 
